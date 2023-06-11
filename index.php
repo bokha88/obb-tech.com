9c995da50174568a91e5866a882e16624efab8d6
Bakhodir Ochilov <bokha621144@gmail.com>
2023-06-11 19:45:19 -0400

    asda

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=devise-width", initial-scale="1.0"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="index.css">
+    <title>New Navbar</title>
+</head>
+<body>
+<header>
+    <div class="navbar">
+        <div class="logo"><a href="#">Web Dev</a></div>
+        <ul class="links">
+            <li><a href="hero">Home</a></li>
+            <li><a href="About">About</a></li>
+            <li><a href="Service">Service</a></li>
+            <li><a href="Contact">Contact</a></li>
+        </ul>
+        <a href="#" class="action-btn">Get Started</a>
+        <div class="toggle_btn">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+    </div>
+</header>
+
+</body>
+</html>
